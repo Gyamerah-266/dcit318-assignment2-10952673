13461ffa1188350020794610f67e2a1bfd75cb3b
Gyamerah-266 <102665481+Gyamerah-266@users.noreply.github.com>
2024-06-15 08:27:03 +0000

    Implement Abstract Classes and Methods

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace OOPDemo
+namespace OOPD
 {
     class Program
     {
@@ -14,6 +14,12 @@ namespace OOPDemo
 
             Cat cat = new Cat();
             cat.MakeSound();
+
+            Circle circle = new Circle(5);
+            Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+
+            Rectangle rectangle = new Rectangle(4, 7);
+            Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
         }
     }
 
@@ -40,5 +46,41 @@ namespace OOPDemo
             Console.WriteLine("Meow");
         }
     }
-}
 
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double GetArea()
+        {
+            return width * height;
+        }
+    }
+}
